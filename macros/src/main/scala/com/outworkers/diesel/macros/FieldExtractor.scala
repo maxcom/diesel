8bb4470cdb4334f3fa81783d422bdee6a6714976
Flavian Alexandru <flavian@outworkers.com>
2016-10-04 08:06:08 +0100

    Adding basic macro defintoion to extract the types.

---
+++ macros/src/main/scala/com/outworkers/diesel/macros/FieldExtractor.scala
@@ -0,0 +1,31 @@
+package com.outworkers.diesel.macros
+
+trait FieldExtractor[Table, Col] {
+  def fields: Seq[Col]
+}
+
+object FieldExtractor {
+  implicit def extractor[T] = macro FieldExtractorMacro.fieldsImpl[T]
+}
+
+@macrocompat.bundle
+class FieldExtractorMacro(val c: scala.reflect.macros.blackbox.Context) {
+  import c.universe._
+
+  def fieldsImpl[Table : c.WeakTypeTag, Col : c.WeakTypeTag]: Tree = {
+    val tpe = weakTypeOf[Table]
+    val colTpe = weakTypeOf[Col]
+    val members: Seq[c.Symbol] = (for {
+      baseClass <- tpe.baseClasses
+      symbol <- baseClass.typeSignature.members.sorted
+      if symbol.typeSignature <:< c.typeOf[Col]
+    } yield symbol)(collection.breakOut)
+
+    val objects = members.distinct.map(_.companion.name)
+
+    q"""new com.outworkers.diesel.macros.FieldExtractor[$tpe, $colTpe] {
+       def fields: Seq[$colTpe] = {Seq(..$objects)}
+    }"""
+  }
+}
+
